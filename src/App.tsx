96211f1b1f82a4dc51eeebff20cdb49895e8a4d0
dundic <tadaleck@gmail.com>
2024-10-12 13:43:20 +0300

    Deploy testing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Testing page for deploying to GitHubPages</h1>
+      <h1>Testing page for deploying to GitHubPages. Deploy test</h1>
     </div>
   );
 }
